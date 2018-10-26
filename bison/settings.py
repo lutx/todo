05b65f4c0fef633d6d9c8146c4cfa58ba40bb360
lukaszu <lukaszburmax@gmail.com>
2018-10-26 20:35:09 +0200

    cos

--- bison/settings.py
+++ bison/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #SECRET_KEY = 'okl$tt=6!=dleq7wsfje6$37chohfn=a1msk!tk7b77#g&qez!'
 SECRET_KEY= config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=False)
+DEBUG = config('DEBUG',default=False )
 
 ALLOWED_HOSTS = []
 
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'bison.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default':dj_database_url.config(default=config(DATABASE_URL)) 
+    'default':DATABASE_URL.config(default=config('DATABASE_URL')) 
     #{
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        
-    #}
+     #   'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      #  'NAME': 'bison',
+       #'HOST': '',
+        #'PORT': '',
+    }
 }
 
 
--- bison/urls.py
+++ bison/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',views.index,name='index'),
+    path('/',views.index,name='todolist'),
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
