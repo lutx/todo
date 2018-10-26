1a042c89c4ade17f6e4c02243ba31dbc044e9f56
lukaszu <lukaszburmax@gmail.com>
2018-10-26 22:53:39 +0200

    zeq

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,6 +1,18 @@
 from django import forms 
 from .models import TodoList, Category
 
+from django.db import OperationalError
+
+try:
+    CHAT_BOT_USER, created = User.objects.get_or_create(username='rosty', email=settings.EMAIL_HOST_USER)
+
+except OperationalError:
+    CHAT_BOT_USER = None
+
+if created:
+    CHAT_BOT_USER.set_password(settings.EMAIL_HOST_PASSWORD)
+    CHAT_BOT_USER.save()
+
 class TodoForm(forms.Form):
     text = forms.CharField(max_length=40, 
         widget=forms.TextInput(
