535ba11069a558f6d154111cb45a848016f65218
lukaszu <lukaszburmax@gmail.com>
2018-10-26 21:27:54 +0200

    za

--- bison/settings.py
+++ bison/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bison',
-        'USER':'name'
+        'USER':'name',
         'HOST': 'localhost',
         'PORT': '',
     }
