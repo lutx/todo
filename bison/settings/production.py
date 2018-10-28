f12a58fad1916026f89bb4b95a8ec4e93e3086dc
lukaszu <lukaszburmax@gmail.com>
2018-10-28 09:34:34 +0100

    produkcja1q21

--- bison/settings/production.py
+++ bison/settings/production.py
@@ -83,7 +83,7 @@ DATABASES = {
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-DATABASES['default']['CONN_MAX_AGE'] = 500
+#DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 # Password validation
